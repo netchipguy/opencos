819621f2502d01feaa1314e9d23157e4cfb2f851
Simon L. Sabato <netchipguy@gmail.com>
2024-02-14 20:55:15 -0800

    Moved the call to 'eda flist' from build.tcl into build.sh.  Calling external tools from within Vivado seems broken on Ubunutu at least (it keeps trying to run a Python from Vivado install area).

--- boards/u200/build.tcl
+++ boards/u200/build.tcl
@@ -35,29 +35,7 @@ set tt "# ********************************"
 set tt "# add top level design files"
 set tt "# ********************************"
 
-# leverage eda to generate an flist for everything under oc_chip_top
-# TBD: does this same call get userspace app?  how?
-# TBD: think about whether this is the right way to go, or whether "eda build" should build flist then run this script.
-#      that approach would enable multiple targets in DEPS that build various flavors of the board, a convenient way
-#      to distribute a bunch of preconfigured boards (i.e. various self-tests, example apps, etc)
-# TBD: another way would be to have eda build command which can take direction via DEPS to launch a TCL and build a command
-#      line for it.  Can this be 
-exec python ${oc_root}/bin/eda flist oc_chip_top --out "[pwd]/build.flist" --force \
-    +define+SYNTHESIS \
-    +define+OC_LIBRARY_XILINX \
-    +define+OC_LIBRARY_ULTRASCALE_PLUS \
-    +define+OC_BOARD_TOP_DEBUG \
-    +define+OC_UART_CONTROL_INCLUDE_VIO_DEBUG \
-    +define+OC_UART_CONTROL_INCLUDE_ILA_DEBUG \
-    --no-emit-incdir \
-    --no-quote-define \
-    --prefix-define "oc_set_project_define " \
-    --prefix-sv "add_files -norecurse " \
-    --prefix-v "add_files -norecurse " \
-    --prefix-vhd "add_files -norecurse "
-
 source build.flist
-
 
 set tt "# ********************************"
 set tt "# check oc_chip_top is the top"
